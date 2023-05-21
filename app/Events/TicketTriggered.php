f287e3a8a951bbea9d0b0c688a3e36dbf29180e2
farrukhjon <nazfar1988@gmail.com>
2023-05-22 01:02:42 +0500

    add pusher

--- app/Events/TicketTriggered.php
+++ app/Events/TicketTriggered.php
@@ -2,24 +2,27 @@
 
 namespace App\Events;
 
+use App\Models\MonitorGroups;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class TicketTriggered implements ShouldBroadcast
 {
     public $tickets;
-    private $monitor_group_id;
+    private string $monitorName;
     private string $eventName = 'ticket_triggered';
 
     public function __construct($tickets)
     {
         $this->tickets = $tickets;
-        $this->monitor_group_id = $this->tickets->firstOrFail()?->monitor_group_id;
+        $ticket = $this->tickets->firstOrFail();
+        $this->monitorName = MonitorGroups::whereId($ticket->monitor_group_id)->firstOrFail()?->name;
+
     }
 
     public function broadcastOn()
     {
-        return new Channel('monitor_group_id_' . $this->monitor_group_id);
+        return new Channel('monitor_group_name_' . $this->monitorName);
     }
 
     public function broadcastAs()
